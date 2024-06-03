27995b52e29d026e45397be3a74410dedfd045ac
NightCrawler040 <birgan2000@gmail.com>
2024-06-03 02:27:19 +0000

    first code commit

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 import requests
 import json
+from faker import Faker
+
 
 APIHOST = "http://library.demo.local"
 LOGIN = "cisco"
@@ -8,7 +12,7 @@ PASSWORD = "Cisco123!"
 def getAuthToken():
     authCreds = (LOGIN, PASSWORD)
     r = requests.post(
-        f"{APIHOST}/api/v1/loginViaBasic",
+        f"{APIHOST}/api/v1/loginViaBasic", 
         auth = authCreds
     )
     if r.status_code == 200:
@@ -16,41 +20,29 @@ def getAuthToken():
     else:
         raise Exception(f"Status code {r.status_code} and text {r.text}, while trying to Auth.")
 
-def deleteBook(book_id, apiKey):
-    r = requests.delete(
-        f"{APIHOST}/api/v1/books/{book_id}",
+def addBook(book, apiKey):
+    r = requests.post(
+        f"{APIHOST}/api/v1/books", 
         headers = {
             "Content-type": "application/json",
             "X-API-Key": apiKey
             },
+        data = json.dumps(book)
     )
     if r.status_code == 200:
-        print(f"Book {book_id} deleted.")
+        print(f"Book {book} added.")
     else:
-        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to delete book {book_id}.")
-
-def getAllBooks(apiKey):
-    r = requests.get(
-        f"{APIHOST}/api/v1/books",
-        headers = {
-            "Content-type": "application/json",
-            "X-API-Key": apiKey
-            },
-    )
-    if r.status_code == 200:
-        return r.json()
-    else:
-        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to get all books.")
+        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
 
 # Get the Auth Token Key
 apiKey = getAuthToken()
 
-# Get all books
-books = getAllBooks(apiKey)
-
-# Get the IDs of the first five and the last five books
-book_ids_to_delete = [book['id'] for book in books[:5]] + [book['id'] for book in books[-5:]]
-
-# Delete the selected books
-for book_id in book_ids_to_delete:
-    deleteBook(book_id, apiKey)
+# Using the faker module, generate random "fake" books
+fake = Faker()
+for i in range(7, 26):
+    fakeTitle = fake.catch_phrase()
+    fakeAuthor = fake.name()
+    fakeISBN = fake.isbn13()
+    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+    # add the new random "fake" book using the API
+    addBook(book, apiKey) 
